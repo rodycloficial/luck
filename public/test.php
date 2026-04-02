59c6389ab70faa0284f3433107e05549c0c992b1
Rod IC <i2414151@continental.edu.pe>
2026-04-01 22:39:49 -0500

    test

---
+++ public/test.php
@@ -0,0 +1,15 @@
+<?php
+ini_set('display_errors', 1);
+error_reporting(E_ALL);
+
+require_once __DIR__ . '/../vendor/autoload.php';
+$app = require_once __DIR__ . '/../bootstrap/app.php';
+
+$kernel = $app->make(Illuminate\Contracts\Http\Kernel::class);
+
+$response = $kernel->handle(
+    $request = Illuminate\Http\Request::capture()
+);
+
+$response->send();
+$kernel->terminate($request, $response);

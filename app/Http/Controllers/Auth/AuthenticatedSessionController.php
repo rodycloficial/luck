defbbf958eb5d565170c27a43366f2b7599a50fd
Rod IC <i2414151@continental.edu.pe>
2026-04-01 23:52:28 -0500

    30

--- app/Http/Controllers/Auth/AuthenticatedSessionController.php
+++ app/Http/Controllers/Auth/AuthenticatedSessionController.php
@@ -11,40 +11,29 @@ use Illuminate\View\View;
 
 class AuthenticatedSessionController extends Controller
 {
-    /**
-     * Display the login view.
-     */
     public function create(): View
     {
         return view('auth.login');
     }
 
-    /**
-     * Handle an incoming authentication request.
-     */
     public function store(LoginRequest $request): RedirectResponse
     {
         $request->authenticate();
 
         $request->session()->regenerate();
+        
         if ((int) Auth::user()->id_rol === 1) {
             return redirect()->intended(route('admin.dashboard'));
         }
-
-        // Si es un usuario normal va a la interfaz inicial
+        
         return redirect()->intended('/');
     }
-    /**
-     * Destroy an authenticated session.
-     */
+
     public function destroy(Request $request): RedirectResponse
     {
         Auth::guard('web')->logout();
-
         $request->session()->invalidate();
-
         $request->session()->regenerateToken();
-
         return redirect('/');
     }
 }

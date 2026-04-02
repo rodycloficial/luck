7b374720f4cda942dca8959282718dbb730cc394
Rod IC <i2414151@continental.edu.pe>
2026-04-01 22:05:11 -0500

    8

--- start.sh
+++ start.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 a2dismod mpm_event
 a2enmod mpm_prefork
-sed -i "s/Listen 80/Listen ${PORT:-8080}/g" /etc/apache2/ports.conf
-sed -i "s/:80/:${PORT:-8080}/g" /etc/apache2/sites-available/000-default.conf
-service apache2 restart
+sed -i 's/Listen 80/Listen 8080/g' /etc/apache2/ports.conf
+sed -i 's/:80/:8080/g' /etc/apache2/sites-available/000-default.conf
 apache2-foreground
